945a266f50e0a741ce0a8cc786bfc6eca246acdd
oliveros <oliveros@oliveros-VirtualBox>
2017-01-24 22:33:28 -0400

    Fix Conflict

--- funcion.py
+++ funcion.py
@@ -13,6 +13,8 @@ class Tarifa:
     _semana = 0.0
     _finde = 0.0
     def __init__(self, sem, fin):
+        if sem < 0 or fin < 0:
+            raise Exception('No existen las Tarifas Negativas')
         self._semana = sem
         self._finde = fin
         
@@ -32,14 +34,15 @@ def calcularPrecio(tarifa, tiempoDeServicio):
     if resta > timedelta(days = 7):
         raise Exception('No puede ser mayor a 7 dias')
     
-    # Pasar todo fin de semana. TO DO falta mas 
-    if tiempoDeServicio[1].weekday() < tiempoDeServicio[0].weekday():
+    # Pasar todo fin de semana.
+    if tiempoDeServicio[1].weekday() <= tiempoDeServicio[0].weekday() and tiempoDeServicio[0].weekday() < 5:
         deltaTiempoSemana = (resta - timedelta(hours = 48)).total_seconds()
         deltaTiempoFinde = timedelta(hours = 48).total_seconds()
     # Iniciar Semana y LLegar a Fin de semana
     elif tiempoDeServicio[0].weekday() < 5 and tiempoDeServicio[1].weekday() >= 5:
         deltaTiempoFinde = timedelta(hours=tiempoDeServicio[1].hour(), minutes=tiempoDeServicio[1].minute(), 
                                          seconds=tiempoDeServicio[1].second(), microseconds=tiempoDeServicio[1].microsecond())
+        # Como se que fue a sabado o domingo, se que si fue a domingo pase todo sabado
         if tiempoDeServicio[1].weekday() == 6:
             deltaTiempoFinde = deltaTiempoFinde + timedelta(days=1)
         deltaTiempoSemana = (resta - deltaTiempoFinde).total_seconds()
@@ -48,7 +51,8 @@ def calcularPrecio(tarifa, tiempoDeServicio):
     elif tiempoDeServicio[0].weekday() >= 5 and tiempoDeServicio[1].weekday() < 5:
         deltaTiempoFinde = timedelta(hours=tiempoDeServicio[0].hour(), minutes=tiempoDeServicio[1].minute(), 
                                          seconds=tiempoDeServicio[0].second(), microseconds=tiempoDeServicio[0].microsecond())
-        if tiempoDeServicio[0].weekday() == 6:
+        # Como llegue a la semana e inicie el sabado, se que pase el domingo
+        if tiempoDeServicio[0].weekday() == 5:
             deltaTiempoFinde = deltaTiempoFinde + timedelta(days=1)
         deltaTiempoSemana = (resta - deltaTiempoFinde).total_seconds()
         deltaTiempoFinde = deltaTiempoFinde.total_seconds()
@@ -79,4 +83,5 @@ if __name__ == '__main__':
     print(a.getFinde())
     calcularPrecio(a, [datetime.today(), datetime(2017, 1, 25)])
     calcularPrecio(a, [datetime.today(), datetime(2017, 2, 23)])
+# csm con palma
     
