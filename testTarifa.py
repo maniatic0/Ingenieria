33758475d9bd59e938639564f2f7f9530cd8f1e2
alexander <alexander@alexander-VirtualBox>
2017-01-25 15:11:01 -0400

    Tests de Tarifa

--- testTarifa.py
+++ testTarifa.py
@@ -4,22 +4,21 @@ Created on Jan 25, 2017
 @author: alexander
 '''
 import unittest
-
+from funcion import Tarifa
 
 class Test(unittest.TestCase):
+    
+    def testTarifaExists(self):
+        Tarifa(0,0)
 
-
-    def setUp(self):
-        pass
-
-
-    def tearDown(self):
-        pass
-
-
-    def testName(self):
-        pass
-
+    def testTarifaExistsSemana(self):
+        t = Tarifa(1,0)
+        self.assertEqual(t.getSemana()==1,True)
+        
+    def testTarifaExistsFinde(self):
+        t = Tarifa(0,1)
+        self.assertEqual(t.getFinde()==1, True)
+        
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
