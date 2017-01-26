834def3c885fb94b526741519ce7b495d230aebc
alexander <alexander@alexander-VirtualBox>
2017-01-25 21:23:42 -0400

    Tests de Excepciones Agregados

--- pruebasFuncion.py
+++ pruebasFuncion.py
@@ -28,11 +28,27 @@ class Test(unittest.TestCase):
         self.assertEqual(calcularPrecio(self.tarifaPrueba, tiempo), 2)
         
     def testExcepcionPasado(self):
+        with self.assertRaises(Exception) as context:
+            a = Tarifa(0,0)
+            tiempo = [datetime(2017, 1, 28), datetime(2017, 1, 27)]
+            calcularPrecio(a, tiempo)
+        self.assertTrue('No puedes ir al pasado' in str(context.exception))
         
     def testExcepcionMinimo(self):
-        
+        with self.assertRaises(Exception) as context:
+            a = Tarifa(0,0)
+            tiempo = [datetime(2017, 1, 28), datetime(2017, 1, 28) + timedelta(minutes=14)]
+            calcularPrecio(a, tiempo)
+        self.assertTrue('No puede ser menor a 15 minutos' in str(context.exception))
+    
     def testExcepcionMaximo(self):
-
+        with self.assertRaises(Exception) as context:
+            a = Tarifa(0,0)
+            tiempo = [datetime(2017, 1, 28), datetime(2017, 2, 5)]
+            calcularPrecio(a, tiempo)
+        self.assertTrue('No puede ser mayor a 7 dias' in str(context.exception))
+        
+    
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
